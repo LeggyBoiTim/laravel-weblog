ab89068dd54ee1d87e42a8524710adf314610459
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2026-02-26 12:11:29 +0100

    Added image model, started with migrations

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Article extends Model
 {
+    public function image(): HasOne
+    {
+        return $this->hasOne(Image::class);
+    }
+
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
